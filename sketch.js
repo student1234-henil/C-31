3c0d29e59645590a5fe98790f56a23cedc84716e
student1234-henil <sali.newtech@gmail.com>
2020-06-15 16:02:24 +0530

    added background

--- sketch.js
+++ sketch.js
@@ -7,11 +7,12 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var bg = "sprites/bg.png";
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImage();
 }
 
 function setup(){
@@ -45,7 +46,10 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg){
+        background(backgroundImg);
+    }
+    
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -85,4 +89,20 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundImage(){
+var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+var responseJSON = await response.json ();
+var dateTime = responseJSON.datetime
+var hour = dateTime.slice(11,13);
+if(hour>=06&&hour<=19){
+bg = "sprites/bg.png";
+}
+else{
+bg =  "sprites/bg2.jpg"; 
+}
+
+backgroundImg = loadImage(bg);
+
 }
